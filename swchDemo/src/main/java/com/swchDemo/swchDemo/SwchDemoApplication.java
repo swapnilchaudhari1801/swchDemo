aa2b7eeb3bdf22004123389eeb0305dd2af7fa31
Swapnil Chaudhari <swapnil.c17@gmail.com>
2018-08-09 14:17:58 +0530

    Uploading the Basic Project
    This is the project schema downloaded from spring.io

---
+++ swchDemo/src/main/java/com/swchDemo/swchDemo/SwchDemoApplication.java
@@ -0,0 +1,12 @@
+package com.swchDemo.swchDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwchDemoApplication.class, args);
+	}
+}
